efc03f95d09338c16a3e651ec8f3133092cffab3
pontazaricardo <pontaza.ricardo@gmail.com>
2018-04-18 00:36:53 +0800

    Update

--- main/main.c
+++ main/main.c
@@ -7,6 +7,54 @@
 #define _GNU_SOURCE
 #define SIZE_T_MAX ((size_t)(~0UL))
 
+typedef struct int_array
+{
+    int *array;
+    size_t length;
+    size_t capacity;
+} int_array;
+
+void int_array_init(int_array *array)
+{
+    array->array = NULL;
+    array->length = 0;
+    array->capacity = 0;
+}
+
+void int_array_free(int_array *array)
+{
+    free(array->array);
+    array->array = NULL;
+    array->length = 0;
+    array->capacity = 0;
+}
+
+void int_array_push_back(int_array *array, int value)
+{
+    if(array->length == array->capacity)
+    {
+        // Not enough space, reallocate.  Also, watch out for overflow.
+        //int new_capacity = array->capacity * 2;
+        int new_capacity = array->capacity + 1;
+        if(new_capacity > array->capacity && new_capacity < SIZE_T_MAX / sizeof(int))
+        {
+            int *new_array = realloc(array->array, new_capacity * sizeof(int));
+            if(new_array != NULL)
+            {
+               array->array = new_array;
+               array->capacity = new_capacity;
+            }
+            else
+                ; // Handle out-of-memory
+        }
+        else
+            ; // Handle overflow error
+    }
+
+    // Now that we have space, add the value to the array
+    array->array[array->length] = value;
+    array->length++;
+}
 
 
 int main(int argc, char *argv[])
